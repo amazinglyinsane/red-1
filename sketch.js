d945f64731fb1582fc47d142e2a61bf67f4a4d42
amazinglyinsane <99406571+amazinglyinsane@users.noreply.github.com>
2022-02-10 21:20:08 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var rect;
+function setup() {
+  createCanvas(400,400);
+  rect=createSprite(200,200,50,50);
+  rect.shapeColor="red"
+}
+function draw() 
+{
+  background(30);
+drawSprites ()
+}
+
+
+
+
